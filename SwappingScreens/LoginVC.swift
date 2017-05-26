5aeff9a3f92574b8b47b782154ece78eb42e14e1
Lagoiko Nick <lagoiko.nick@gmail.com>
2017-05-27 02:43:45 +0300

    creating seagues

---
+++ SwappingScreens/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  SwappingScreens
+//
+//  Created by Nick on 27.05.17.
+//  Copyright © 2017 LagoikoNick. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+        
+    }
+
+  
+
+}
+
